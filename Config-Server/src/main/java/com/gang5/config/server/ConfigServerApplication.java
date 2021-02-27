0b2c2e160fa30644febea730e1a8ad0ea7ae54e7
srikanthvelugoti <srikanth.velugoti@outlook.com>
2021-02-27 14:43:03 +0530

    added

--- Config-Server/src/main/java/com/gang5/config/server/ConfigServerApplication.java
+++ Config-Server/src/main/java/com/gang5/config/server/ConfigServerApplication.java
@@ -2,7 +2,8 @@ package com.gang5.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
