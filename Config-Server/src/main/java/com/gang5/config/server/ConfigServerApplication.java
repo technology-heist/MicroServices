42e84fff0948309c314e8ca53160c8ea778d675e
srikanthvelugoti <srikanth.velugoti@outlook.com>
2021-02-27 13:07:08 +0530

    config server

---
+++ Config-Server/src/main/java/com/gang5/config/server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.gang5.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
